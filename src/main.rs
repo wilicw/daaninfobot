c420bfb7e1d0e3ec7de090a1b9db0120d63d67e8
William Chang <wilicw1346@gmail.com>
2023-11-23 11:45:59 +0800

    feat: rewrite command parser and add title command

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 use rand::Rng;
-use teloxide::prelude::*;
+use teloxide::{
+    prelude::*,
+    types::{InputFile, MessageEntityKind},
+    utils::command::BotCommands,
+};
 
 const HELP: &str = r#"
 #歡迎光臨洗手室
 /help - 檢視說明
 /roll - 擲骰子
-/dinner [options] - 晚餐吃什麼
+/title user string  - 變更使用者標籤
+/dinner options... - 晚餐吃什麼
     e.g. /dinner 八方雲集 Sukiya 臺鐵便當 元氣
 "#;
+
+#[derive(BotCommands, Clone)]
+#[command(rename_rule = "lowercase")]
+enum Command {
+    Start,
+    Help,
+    Roll,
+    Title,
+    Dinner,
+}
 
 fn dinner(options: String) -> String {
     let arr: Vec<&str> = options.split_whitespace().collect();
@@ -16,23 +31,79 @@ fn dinner(options: String) -> String {
     return arr[index].to_string();
 }
 
-async fn message_parser(msg: &Message, bot: &Bot) {
+async fn message_parser(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
     let full_text = msg.text().unwrap();
-    let command = full_text.split_whitespace().next().unwrap();
-    match command {
-        "/help" => {
-            let _ = bot.send_message(msg.chat.id, HELP).await;
+    dbg!(full_text);
+    match cmd {
+        Command::Start | Command::Help => bot.send_message(msg.chat.id, HELP).await?,
+        Command::Roll => {
+            if rand::thread_rng().gen_range(0..=2) == 0 {
+                bot.send_animation(msg.chat.id, InputFile::file("./rickroll-roll.gif"))
+                    .await?
+            } else {
+                bot.send_dice(msg.chat.id).await?
+            }
         }
-        "/roll" => {
-            let _ = bot.send_dice(msg.chat.id).await;
+        Command::Title => {
+            let mut title: &str = "";
+            let mut user_id: UserId = UserId(0);
+            let mut username: &str = "";
+            for entity in msg.entities().unwrap() {
+                let offset = entity.offset;
+                let length = entity.length;
+                match &entity.kind {
+                    MessageEntityKind::Mention => {
+                        username = full_text.get(offset + 0..offset + length).unwrap();
+                        user_id = UserId(1060366902);
+                        title = full_text.get(offset + length + 1..).unwrap();
+                        break;
+                    }
+                    MessageEntityKind::TextMention { user } => {
+                        username = full_text.get(offset + 0..offset + length).unwrap();
+                        user_id = user.id;
+                        title = full_text.get(offset + length + 1..).unwrap();
+                        break;
+                    }
+                    _ => {}
+                }
+            }
+
+            if title.is_empty() | (user_id == UserId(0)) | username.is_empty() {
+                bot.send_message(
+                    msg.chat.id,
+                    "請輸入選項 e.g. /title user string".to_string(),
+                )
+                .await?
+            } else {
+                bot.promote_chat_member(msg.chat.id, user_id)
+                    .can_change_info(false)
+                    .can_delete_messages(false)
+                    .can_invite_users(true)
+                    .can_restrict_members(false)
+                    .can_pin_messages(true)
+                    .can_promote_members(false)
+                    .await?;
+                bot.set_chat_administrator_custom_title(msg.chat.id, user_id, title)
+                    .await?;
+                bot.send_message(
+                    msg.chat.id,
+                    format!("{} 的標籤已變更為 {}", username, title),
+                )
+                .await?
+            }
         }
-        "/dinner" => {
-            let options = full_text.splitn(2, ' ').nth(1).unwrap();
-            let result = dinner(options.to_string());
-            let _ = bot.send_message(msg.chat.id, result).await;
+        Command::Dinner => {
+            let result: String;
+            if full_text.split_whitespace().count() < 2 {
+                result = "請輸入選項 e.g. /dinner 八方雲集 Sukiya 臺鐵便當 元氣".to_string();
+            } else {
+                result = dinner(full_text.splitn(2, ' ').nth(1).unwrap().to_string());
+            }
+            bot.send_message(msg.chat.id, result).await?
         }
-        _ => {}
-    }
+    };
+
+    Ok(())
 }
 
 #[tokio::main]
@@ -41,10 +112,5 @@ async fn main() {
     log::info!("Starting throw dice bot...");
 
     let bot = Bot::from_env();
-
-    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-        message_parser(&msg, &bot).await;
-        Ok(())
-    })
-    .await;
+    Command::repl(bot, message_parser).await;
 }
